84db972dad6848600405c8847de668689402a591
Sanjan Krishna Sarat <sanjankrishna007@protonmail.com>
2024-11-21 17:04:26 +0800

    Just adding two_player.py into the github

---
+++ two_player.py
@@ -0,0 +1,10 @@
+#This is just a 5x5 grid that will be initialized at the start of each game
+#o (not 0) means miss and x means a hit
+grid = [
+    [' '], [' '], [' '], [' '], [' ']
+    [' '], [' '], [' '], [' '], [' ']
+    [' '], [' '], [' '], [' '], [' ']
+    [' '], [' '], [' '], [' '], [' ']
+    [' '], [' '], [' '], [' '], [' ']
+]
+
