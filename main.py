d1f4f630507bdcd974aea8aa0f9700334ab54619
Bryan Chua <bcbryanchua@gmail.com>
2024-11-28 17:02:49 +0800

    Co-authored-by: Brand0n11 <Brand0n11@users.noreply.github.com> Co-authored-by: Pangnocode <Pangnocode@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-from singleplayerai import draw_square
+from tkinter import *
+import tkinter as tk
+from tkinter import messagebox as msb
 
-# Call the draw_square function
-draw_square()
+class main(object):
+    time = 0
+
+    def __init__(self):
+         
+        self.__root = Tk()
+        self.__root.title("Battleship")
+
+        APP_MIN_WIDTH = 1000
+        APP_MIN_HEIGHT = 600
+        APP_MAX_WIDTH = 1000
+        APP_MAX_HEIGHT = 600
+        self.__root.minsize(APP_MIN_WIDTH,
+                            APP_MIN_HEIGHT)
+        self.__root.maxsize(APP_MAX_WIDTH,
+                            APP_MAX_HEIGHT)
+        self.__root.protocol("WM_DELETE_WINDOW", self.on_exit_button_pressed)
+
 
--- placing_ships_player.py
+++ placing_ships_player.py
@@ -1,4 +1,3 @@
-
 # Making grid with empty spaces
 
 empty_grid5x5 = {
@@ -10,22 +9,59 @@ empty_grid5x5 = {
 }
 
 # Making player choose initial square
-row_select = input("Choose a row to place the ship.")  
+
+row_select = input("Choose a row to place the ship.")
+while int(row_select) < 1 or int(row_select) > 5:
+    row_select = input("Choose a different row to place the ship.")
 column_select = input("Choose a column to place the ship.")
+while int(column_select) < 1 or int(column_select) > 5:
+    column_select = input("Choose a different column to place the ship.")
 
-# Placing the initial piece
+# Choosing Final Piece
 
-def grid_select(a,b):
-    row = str(a)
-    column = int(b) - 1
-    empty_grid5x5[row][column] = 'X'
+final_row_select = input("Choose a row to finish the ship.")
+while int(final_row_select) < 1 or int(final_row_select) > 5:
+    final_row_select = input("Choose a different row to finish the ship.")
+while abs(int(row_select) - int(final_row_select)) > 4:
+    final_row_select = input("Choose a different row to finish the ship.")
+final_column_select = input("Choose a column to finish the ship.")
+while int(final_column_select) < 1 or int(final_column_select) > 5:
+    final_column_select = input("Choose a different column to finish the ship.")
+while abs(int(column_select) - int(final_column_select)) > 4:
+    final_column_select = input("Choose a different column to finish the ship.")
+
+# Placing the ship
+
+def ship_placing(a,b):
+    ship_length = 0
+    if int(final_row_select) > int(row_select) and int(final_column_select) == int(column_select):
+        for i in range(int(row_select),int(final_row_select)+1):
+            row = str(i)
+            column = int(final_column_select) - 1
+            empty_grid5x5[row][column] = 'X'
+            ship_length += 1
+    if int(final_row_select) < int(row_select) and int(final_column_select) == int(column_select):
+        for i in range(int(final_row_select),int(row_select)+1):
+            row = str(i)
+            column = int(final_column_select) - 1
+            empty_grid5x5[row][column] = 'X'
+            ship_length += 1
+    if int(column_select) < int(final_column_select) and int(final_row_select) == int(row_select):
+        for i in range(int(column_select) - 1,int(final_column_select)):
+            column = i
+            row = a
+            empty_grid5x5[row][column] = 'X'
+            ship_length += 1
+    if int(column_select) > int(final_column_select) and int(final_row_select) == int(row_select):
+        for i in range(int(final_column_select) - 1,int(column_select)):
+            column = i
+            row = a
+            empty_grid5x5[row][column] = 'X'
+            ship_length += 1
     return empty_grid5x5
 
-placed_grid = grid_select(row_select,column_select)
+# Printing the Ship
 
-# Choosing orientation to finish the ship 
-
-direction = input("Choose a direction to place the ship") # For now use NSEW, can decide on this later
-while direction.isalpha() == False:
-    direction = input("Please choose North, South, East or West")
-direction = direction.title()
+placed_grid = ship_placing(final_row_select,final_column_select)
+for i in placed_grid:
+    print(empty_grid5x5[i])
